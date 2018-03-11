4f211352c05d1a26fa54b34ed99aa3de745e72d4
krishna4a6 <krishanaprasad18@gmail.com>
2018-03-11 11:17:15 +0530

    jbjguh

--- start/src/app/app.module.ts
+++ start/src/app/app.module.ts
@@ -1,15 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { GrowlModule } from 'primeng/primeng';
+import { ChartModule } from 'primeng/chart';
+
 import { AppComponent } from './app.component';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ChartModule,
+    GrowlModule
   ],
+  
   providers: [],
   bootstrap: [AppComponent]
 })
